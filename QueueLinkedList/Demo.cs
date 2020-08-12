862161fb3ca4fc39cddd1998caeade5fa18f17c4
MazaharAhamed <mazaharahamed77@gmail.com>
2020-08-12 21:29:50 +0530

    Add project files.

---
+++ QueueLinkedList/Demo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueLinkedList
+{
+    class Demo
+    {
+        static void Main(String[] args)
+        {
+            int choice, x;
+
+            QueueL quL = new QueueL();
+
+            while(true)
+            {
+                Console.WriteLine("1.Queue Size");
+                Console.WriteLine("2.Insert Element in Queue");
+                Console.WriteLine("3.Delete Element in Queue");
+                Console.WriteLine("4.Front Element in Queue");
+                Console.WriteLine("5.Display all elements of Queue");
+                Console.WriteLine("6.Quit");
+                Console.WriteLine("Enter Your Choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 6)
+                    break;
+
+                switch(choice)
+                {
+                    case 1:
+                        Console.WriteLine("Size of Queue is: " + quL.Size());
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter element to insert:");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        quL.Insert(x);
+                        break;
+                    case 3:
+                        Console.WriteLine("Deleted Element is: "+quL.Delete());
+                        break;
+                    case 4:
+                        Console.WriteLine("Front Element in Queue is: " + quL.Peek());
+                        break;
+                    case 5:
+                        quL.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice");
+                        break;
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ QueueLinkedList/QueueL.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Xml;
+
+namespace QueueLinkedList
+{
+    class QueueL
+    {
+        private Node front;
+        private Node rear;
+
+        public QueueL()
+        {
+            front = null;
+            rear = null;
+        }
+
+        public int Size()
+        {
+            int s = 0;
+            Node p = front;
+            while(p!=null)
+            {
+                s++;
+                p = p.link;
+            }
+            return s;
+        }
+
+        public void Insert(int x)
+        {
+            Node temp = new Node(x);
+
+            if (front == null)                        /*If Queue is Null*/
+                front = temp;
+            else
+                rear.link = temp;
+            rear = temp;
+        }
+
+        public int Delete()
+        {
+            int x;
+            if (isEmpty())
+                throw new System.InvalidOperationException("Queue Underflow");
+            x = front.info;
+            front = front.link;
+            return x;
+        }
+
+        public int Peek()
+        {
+            if (isEmpty())
+                throw new System.InvalidOperationException("Queue Underflow");
+            return front.info;
+        }
+
+        public bool isEmpty()
+        {
+            return (front == null);
+        }
+
+        public void Display()
+        {
+            if (isEmpty())
+                Console.WriteLine("Queue is Empty");
+            Console.WriteLine("Queue is");
+            Node p = front;
+            while(p!=null)
+            {
+                Console.WriteLine(p.info + " ");
+                p = p.link;
+            }
+            Console.WriteLine();
+        }
+    }
+}
